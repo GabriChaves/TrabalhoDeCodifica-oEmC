0bced9c3af37adb1981142bd18bc0a4e2f34fae1
Gabriel Chaves <bielbahia0307@gmail.com>
2025-04-02 11:36:35 -0300

    Resoluções: PUCRS - Lista de Exercícios

---
+++ Lista-PUCRS-Exercício4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int anos = (150 - 110) / (3 - 2);
+    printf("Zé será maior que Chico em %d anos.\n", anos);
+    
+    //A diferença inicial de altura é 150 cm - 110 cm = 40 cm
+    //Zé ganha 1 cm por ano a mais que Chico (3 cm - 2 cm = 1 cm)
+    //Portanto, 40 cm / 1 cm/ano = 40 anos
+    
+    return 0;
+}
