0f9c5d4f225d2e0a866f8a24612cdd1abd567096
Gabriel Chaves <bielbahia0307@gmail.com>
2025-05-26 11:02:25 -0300

    Create Lista-IFSC-Exercício053.c

---
+++ IFSC/Lista-IFSC-Exercício053.c
@@ -0,0 +1,12 @@
+int main() {
+    int a, b, c;
+    printf("Digite três números: ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if (a < b) { int tmp = a; a = b; b = tmp; }
+    if (a < c) { int tmp = a; a = c; c = tmp; }
+    if (b < c) { int tmp = b; b = c; c = tmp; }
+
+    printf("Ordem decrescente: %d %d %d\n", a, b, c);
+    return 0;
+}
